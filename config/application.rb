d799617781a8ea29236a0e15f617d33eb71d1bdd
Jennifer Gifford <jgifford82@gmail.com>
2023-02-13 11:03:02 -0700

    undo Bundler.reuqire and comment out sprockets/railtie

--- config/application.rb
+++ config/application.rb
@@ -13,12 +13,11 @@ require "action_controller/railtie"
 require "action_view/railtie"
 # require "action_cable/engine"
 require "rails/test_unit/railtie"
-require 'sprockets/railtie'
+# require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
-Bundler.require(:default, :assets, Rails.env)
+Bundler.require(*Rails.groups)
 
 module BirdApp
   class Application < Rails::Application

11c78ecf2f3145a000be3666624db41ba1191d46
Jennifer Gifford <jgifford82@gmail.com>
2023-02-13 10:55:50 -0700

    added back previoulsy deleted lines; updated Bundler.require

--- config/application.rb
+++ config/application.rb
@@ -13,11 +13,12 @@ require "action_controller/railtie"
 require "action_view/railtie"
 # require "action_cable/engine"
 require "rails/test_unit/railtie"
-# require 'sprockets/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+Bundler.require(:default, :assets, Rails.env)
 
 module BirdApp
   class Application < Rails::Application
